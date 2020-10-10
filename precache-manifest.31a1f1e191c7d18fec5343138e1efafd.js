9a7342041f5d54a83bee82af4862147dd209c68f
Ahamed <ahamedabdul24@gmail.com>
2020-10-10 17:41:39 +0530

    Updates

---
+++ precache-manifest.31a1f1e191c7d18fec5343138e1efafd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ce47ec715d2f56ef79eebfc160a823",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "727fef8959c9937d3e14",
+    "url": "/chatbot/static/css/main.0fd5cf0a.chunk.css"
+  },
+  {
+    "revision": "11bd68707197db79a1c3",
+    "url": "/chatbot/static/js/2.643c6ba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatbot/static/js/2.643c6ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727fef8959c9937d3e14",
+    "url": "/chatbot/static/js/main.479098cd.chunk.js"
+  },
+  {
+    "revision": "6da786bfa225ae5459b9",
+    "url": "/chatbot/static/js/runtime-main.253fb56a.js"
+  }
+]);
